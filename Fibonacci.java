cdc81bb0464149cf92edba2336074256677b73af
pranathi-39 <pranathi150807@gmail.com>
2025-02-24 22:53:52 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -4,10 +4,10 @@ public class Fibonacci {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number of terms for the Fibonacci series: ");
-        int terms = scanner.nextInt();
+        int n = scanner.nextInt();
         int a = 0, b = 1;
         System.out.print("Fibonacci Series: ");
-        for (int i = 1; i <= terms; i++) {
+        for (int i = 1; i <= n; i++) {
             System.out.print(a + " ");
             int nextTerm = a + b;
             a = b;
